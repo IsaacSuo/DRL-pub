e6f8cf82e82e5114b4ab7fadc6dbaa59e202261e
IsaacSuo <suofang000@gmail.com>
2025-11-06 15:36:38 +0800

    要提交的变更： 	新文件：   config/dqn_cfg.py 	新文件：   model/dqn_mlp.py 	新文件：   policy/dqn.py 	修改：     trainer.py
     未跟踪的文件:
    	logs/
    	results/dqn/

--- trainer.py
+++ trainer.py
@@ -7,11 +7,14 @@ import os
 
 from config.train import TrainingConfig
 from config.ddqn_cfg import DDQNConfig
+from config.dqn_cfg import DQNConfig
 from policy.base import BasePolicy, DoneException
 from agent.core import CoreAgent
 from agent.kytolly import KytollyAgent
 from policy.ddqn import DoubleDeepQNetworkPolicy
+from policy.dqn import DQNPolicy
 from model.ddqn_mlp import DoubleDeepQNetworkModel
+from model.dqn_mlp import DeepQNetworkModel
 
 class Trainer():
     def __init__(self, device, log_dir,):
@@ -25,7 +28,7 @@ class Trainer():
     def train(self):
         '''默认中每个 baseline approach 都应该使用相同的 TrainingConfig'''
     def train_kytolly(self, train_cfg, env, cb):
-        online_ddqn_cfg = DDQNConfig(hidden_dims=[128, 128], metrics=['mse, loss'])
+        online_ddqn_cfg = DDQNConfig(hidden_dims=[128, 128])
         ddqn_model = DoubleDeepQNetworkModel(online_ddqn_cfg)
         xqy_policy = DoubleDeepQNetworkPolicy(model=ddqn_model,device=self.device)
         xqy_agent = KytollyAgent(env, xqy_policy, train_cfg, cb)
@@ -40,4 +43,25 @@ class Trainer():
         fig1.savefig('results/ddqn/8/train_cfg.png')
         fig2.savefig('results/ddqn/8/plot_smoothed_training_rwd.png')
         fig3.savefig('results/ddqn/8/plot_eval_rwd_var.png')
+
+    def train_dqn(self, train_cfg, env, cb):
+        '''使用标准DQN算法进行训练'''
+        dqn_cfg = DQNConfig(hidden_dims=[128, 128])
+        dqn_model = DeepQNetworkModel(dqn_cfg)
+        dqn_policy = DQNPolicy(model=dqn_model, device=self.device)
+        dqn_agent = KytollyAgent(env, dqn_policy, train_cfg, cb)
+        dqn_agent.learn()
+
+        fig0, ax0 = dqn_cfg.table()
+        fig1, ax1 = train_cfg.table()
+        fig2, ax2 = dqn_agent.plot_smoothed_training_rwd()
+        fig3, ax3 = dqn_agent.plot_eval_rwd_var()
+
+        # 创建results/dqn目录
+        os.makedirs('results/dqn/8', exist_ok=True)
+
+        fig0.savefig('results/dqn/8/dqn_cfg.png')
+        fig1.savefig('results/dqn/8/train_cfg.png')
+        fig2.savefig('results/dqn/8/plot_smoothed_training_rwd.png')
+        fig3.savefig('results/dqn/8/plot_eval_rwd_var.png')
         
